0dd7e16aa2edf7d283b27c672156de37c8dec1af
André Daniel <andre.fervilha@gmail.com>
2017-08-30 12:58:06 -0300

    Setup angular and materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// materialize
+import { MaterializeModule } from "angular2-materialize";
+
+// components
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +12,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
